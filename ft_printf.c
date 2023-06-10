799267ce9d41526535d6d251621210562067bba3
Nao Sakanoue <nsakanou@c1r23s8.42tokyo.jp>
2023-06-10 14:03:43 +0900

    1

--- ft_printf.c
+++ ft_printf.c
@@ -6,62 +6,43 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:10:03 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/06/09 20:37:00 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/06/10 14:03:29 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include "printf.h"
 
 int	ft_vdprintf(int fd, const char *fmt, va_list ap)
 {
 	ssize_t	i;
-	ssize_t	count;
 
 	i = 0;
-	count = 0;
 	while (fmt[i])
 	{
 		if (fmt[i] != '%')
 		{
 			ft_putchar(fd, fmt[i]);
-			count++;
 			continue ;
 		}
 		i++;
 		if (fmt[i] == '%')
-		{
-			ft_putchar(fd, fmt[i]);
-			count++;
-		}
+			return (ft_putchar(fd, fmt[i]));
 		else if (fmt[i] == 'c')
-		{
-			ft_putchar(fd, va_arg(ap, int));
-			count++;
-		}
+			return (ft_putchar(fd, va_arg(ap, int)));
 		else if (fmt[i] == 's')
-		{
-			const char *str = va_arg(ap, const char *);
-			ft_putstr(fd, str);
-		}
+			return (ft_putstr(fd, va_arg(ap, const char *)));
 		else if (fmt[i] == 'u')
-			count += ft_print_unbr(va_arg(ap, unsigned int));
+			return (ft_print_unbr(va_arg(ap, unsigned int)));
 		else if (fmt[i] == 'x')
-		{
-			unsigned int num = va_arg(ap, unsigned int);
-			ft_print_hex(fd, num);
-		}
+			return (ft_print_hex(fd, va_arg(ap, unsigned int)));
 		else if (fmt[i] == 'X')
-		{
-			unsigned int num = va_arg(ap, unsigned int);
- 			ft_print_hex(fd, num);
-		}
+			return (ft_print_hex(fd, va_arg(ap, unsigned int)));
 		else if (fmt[i] == 'd')
-			count += ft_print_nbr(va_arg(ap, int));
+			return (ft_print_nbr(va_arg(ap, int)));
 		else if (fmt[i] == 'p')
-			ft_ptr(va_arg(ap, void *));
+			return (ft_ptr(va_arg(ap, void *)));
 		else if (fmt[i] == 'i')
-			count += ft_print_nbr(va_arg(ap, int));
+			return (ft_print_nbr(va_arg(ap, int)));
 	}
 	return (count);
 }
@@ -73,6 +54,7 @@ int	ft_printf(const char *fmt, ...)
 
 	va_start(ap, fmt);
 	count = ft_vdprintf(STDOUT_FILENO, fmt, ap);
+	count++;
 	va_end(ap);
 	return (count);
 }
